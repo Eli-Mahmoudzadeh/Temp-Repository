c8412363f48785d2b9dadc61326116adb044c569
Eli-Mahmoudzadeh <76820306+Eli-Mahmoudzadeh@users.noreply.github.com>
2021-05-29 21:44:20 -0700

    Locate a Label whith code writer's Name, Commit and Push and PullRequest

--- MyApplication/Form01.Designer.cs
+++ MyApplication/Form01.Designer.cs
@@ -28,18 +28,34 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.SuspendLayout();
-			// 
-			// Form01
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(484, 461);
-			this.Location = new System.Drawing.Point(0, 0);
-			this.Name = "Form01";
-			this.ResumeLayout(false);
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Font = new System.Drawing.Font("Verdana", 11F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))));
+            this.NameLabel.Location = new System.Drawing.Point(38, 170);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(404, 18);
+            this.NameLabel.TabIndex = 0;
+            this.NameLabel.Text = "Elnaz Mahmoudzadeh have made some changes";
+            // 
+            // Form01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.NameLabel);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "Form01";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
-		#endregion /Windows Form Designer generated code
-	}
+        #endregion /Windows Form Designer generated code
+
+        private System.Windows.Forms.Label NameLabel;
+    }
 }
